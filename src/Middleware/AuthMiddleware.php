80be00ffc4bfbc06781a10b2e62d8eb4fbe9a380
Fambegbe Olamileke <olamileke.f@gmail.com>
2024-03-18 00:22:46 +0100

    feat: working on auth middleware

--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace Motif\Middleware;
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Firebase\JWT\Key;
+
 class LoginMiddleware
 {
-    
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        $authHeader = $request->getHeader("Authorization");
+        return $handler->handle($request);
+    }
 }
